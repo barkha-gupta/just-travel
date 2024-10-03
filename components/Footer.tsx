0d6c81183ee9efe6bb029cf8f356371523e0cf4f
barkha-gupta <barkhagupta401@gmail.com>
2024-10-04 01:50:00 +0530

    feat: complete footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,9 @@ import { FaSquareTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="">
-      <div className="p-4 ">
-        <div className="flex flex-col">
+    <div className="flex flex-col">
+      <div className="p-4 flex flex-col lg:flex-row lg:gap-4 gap-8 justify-between text-sm">
+        <div className="flex flex-col items-center">
           <div className="w-52 h-10">
             <Image src={Logo} alt="company logo" />
           </div>
@@ -25,6 +25,37 @@ const Footer = () => {
             <FaYoutubeSquare size="36" className="fill-red-600" />
 
             <FaSquareTwitter size="36" className="fill-blue-600" />
+          </div>
+        </div>
+        <div className="flex flex-row lg:flex-col gap-3">
+          <div className="font-semibold">Company</div>
+          <div className="text-secondary flex flex-row lg:flex-col gap-2">
+            <div>About Us</div>
+            <div>Contact Us</div>
+            <div>Our Blogs</div>
+            <div>Payment Policy</div>
+          </div>
+        </div>
+
+        <div className="flex flex-row lg:flex-col gap-3">
+          <div className="font-semibold">Trips</div>
+          <div className="text-secondary flex flex-row lg:flex-col gap-2">
+            <div>Yatras</div>
+            <div>Treks</div>
+            <div>Weekend Gateways</div>
+            <div>All Girls Trip</div>
+            <div>Corporate Trips</div>
+          </div>
+        </div>
+
+        <div className="lg:flex lg:flex-col gap-3 lg:py-0 py-4 lg:px-0 px-4 lg:shadow-none lg:bg-white shadow-md bg-[#daefff] rounded-lg">
+          <div className="font-semibold lg:p-0 pb-2">Contact US</div>
+          <div className="font-medium lg:flex lg:flex-col lg:gap-2">
+            <span>B-42, 2nd Floor, Tower - B</span>
+            <span>The Corenthum, Block A, Sector 62 Noida </span>
+            <span>Uttar Pradesh, 201201</span>
+            <div className="lg:pt-0 pt-4">+91-8810 457 631</div>
+            <div className="lg:pt-0 pt-4">contact@justwravel.com</div>
           </div>
         </div>
       </div>
